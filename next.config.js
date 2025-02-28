545818604917173655505d2e38e71be1bf4a7866
SwapMarket <181500791+SwapMarket@users.noreply.github.com>
2025-02-28 17:52:26 +0100

    libssl3

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  swcMinify: false,  // Disable SWC minification
   reactStrictMode: true,
   webpack: (config) => {
     config.resolve.fallback = {
